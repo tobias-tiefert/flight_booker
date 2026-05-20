63cb868a62efa01c13e990ade18712557e8639eb
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2026-05-20 12:02:39 +0200

    Add basic css and styling for head

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,9 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
 
   validates :start_datetime, :duration, presence: true
+
+  scope :from_airport, ->(airport_id) { where(departure_airport_id: airport_id) if airport_id.present? }
+  scope :to_airport, ->(airport_id) { where(arrival_airport_id: airport_id) if airport_id.present? }
+
+  scope :on_date, ->(date) { where(start_datetime: date.to_date.all_day) if date.present? }
 end
