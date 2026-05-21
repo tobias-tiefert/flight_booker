ef02e311f6959d4d2913218c2051371d05ad6a07
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2026-05-21 13:32:53 +0200

    Add Passenger and Booking models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
+  has_many :bookings, dependent: :destroy
+
   validates :start_datetime, :duration, presence: true
 
   scope :from_airport, ->(airport_id) { where(departure_airport_id: airport_id) if airport_id.present? }
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_20_153144) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_21_112633) do
   create_table "airports", force: :cascade do |t|
     t.string "code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "flight_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -29,6 +36,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_20_153144) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.integer "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
+  add_foreign_key "passengers", "bookings"
 end
